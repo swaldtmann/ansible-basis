a696e3b81a077fddd56eff95a69308ef612e1fee
Stephan Waldtmann <stephan@waldtmann.de>
2018-10-08 20:53:01 +0200

    Added git_setup.sh

---
+++ git_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global user.name "Stephan Waldtmann"
+git config --global user.email stephan@waldtmann.de
+git config --global core.editor vi
+git config --global alias.st status
+
+git config --list
