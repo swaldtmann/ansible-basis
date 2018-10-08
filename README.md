cc063cf2a69d14ac4b25652266ef360adc8ff52f
Stephan Waldtmann <stephan@waldtmann.de>
2018-10-08 20:48:18 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Demoumgebung Ansible Playbooks
+
+## Verzeichnisstruktur
+[](inventories/group_vars/all/vault.yml) 
---
+++ run.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i inventories/test/hosts playbooks/test_local.yml --vault-id keep/vault-password-file
